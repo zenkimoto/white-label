9b5228229f6063362fcbb6354d3a88e0cdde6818
Alex Yip <alex.yip@adhocteam.us>
2021-06-11 06:20:20 -0700

    Removed CRA broilerplate

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import NavBar from './navbar';
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <div class="container">
+        <div class="row">
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+function NavBar() {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Client</span>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
